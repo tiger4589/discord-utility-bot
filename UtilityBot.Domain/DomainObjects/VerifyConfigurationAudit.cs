7f5ed8b2712a73069477fd8e8d31c26664810139
Paul Karam <paulkaram1989@hotmail.com>
2023-02-20 22:27:48 +0100

    Added audit tables

---
+++ UtilityBot.Domain/DomainObjects/VerifyConfigurationAudit.cs
@@ -0,0 +1,7 @@
+﻿namespace UtilityBot.Domain.DomainObjects;
+
+public class VerifyConfigurationAudit : BaseAudit
+{
+    public ulong ChannelId { get; set; }
+    public ulong RoleId { get; set; }
+}
